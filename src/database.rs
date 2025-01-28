234c8f2cb2fa57ab7d0a34357a7f065daba9b251
LukeErdy <112845457+LukeErdy@users.noreply.github.com>
2025-01-28 12:53:04 -0800

    feat: Add workflow w/parallel rasterization

--- src/database.rs
+++ src/database.rs
@@ -1,8 +1,8 @@
 use serde::{Deserialize, Serialize};
 use std::fmt::Debug;
 use std::{
-    collections::HashMap,
-    sync::{Mutex, OnceLock},
+	collections::HashMap,
+	sync::{Mutex, OnceLock},
 };
 
 use crate::simulation::*;
@@ -26,79 +26,79 @@ pub type DocID = u32;
 #[allow(non_snake_case)]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct PrintJob {
-    #[serde(default)]
-    id: Option<DocID>,
-    #[serde(default)]
-    DateCreated: Option<u32>,
-    Title: String,
-    pub PageCount: u32,
-    RasterizationProfile: String,
+	#[serde(default)]
+	id: Option<DocID>,
+	#[serde(default)]
+	DateCreated: Option<u32>,
+	Title: String,
+	pub PageCount: u32,
+	RasterizationProfile: String,
 }
 
 #[allow(non_snake_case)]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 //TODO: Different name?
 pub struct WFS {
-    pub id: DocID,
-    pub Prev: Vec<usize>,
-    pub Next: Vec<usize>,
+	pub id: DocID,
+	pub Prev: Vec<usize>,
+	pub Next: Vec<usize>,
 }
 
 #[allow(non_snake_case)]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Workflow {
-    #[serde(default)]
-    id: Option<DocID>,
-    Title: String,
-    pub WorkflowSteps: Vec<WFS>,
+	#[serde(default)]
+	id: Option<DocID>,
+	Title: String,
+	pub WorkflowSteps: Vec<WFS>,
 }
 
 #[allow(non_snake_case)]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct WorkflowStep {
-    #[serde(default)]
-    pub id: Option<DocID>,
-    pub Title: String,
-    pub SetupTime: u32,
-    pub TimePerPage: u32,
+	#[serde(default)]
+	pub id: Option<DocID>,
+	pub Title: String,
+	pub SetupTime: u32,
+	pub TimePerPage: u32,
 }
 
 #[allow(non_snake_case)]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct SimulationReport {
-    #[serde(default)]
-    id: Option<DocID>,
-    PrintJobID: DocID,
-    WorkflowID: DocID,
-    CreationTime: u32,
-    TotalTimeTaken: u32,
-    StepTimes: HashMap<DocID, u32>, //Key: WorkflowStep ID; Value: Total time for that step
+	#[serde(default)]
+	id: Option<DocID>,
+	PrintJobID: DocID,
+	WorkflowID: DocID,
+	CreationTime: u32,
+	TotalTimeTaken: u32,
+	StepTimes: HashMap<DocID, u32>, //Key: WorkflowStep ID; Value: Total time for that step
 }
 
 #[allow(non_snake_case)]
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SimulationReportArgs {
-    pub PrintJobID: DocID,
-    pub WorkflowID: DocID,
+	pub PrintJobID: DocID,
+	pub WorkflowID: DocID,
 }
 
 impl SimulationReport {
-    pub fn new(
-        print_job_id: DocID,
-        workflow_id: DocID,
-        creation_time: u32,
-        total_time_taken: u32,
-        step_times: HashMap<DocID, u32>,
-    ) -> SimulationReport {
-        return SimulationReport {
-            id: None,
-            PrintJobID: print_job_id,
-            WorkflowID: workflow_id,
-            CreationTime: creation_time,
-            TotalTimeTaken: total_time_taken,
-            StepTimes: step_times,
-        };
-    }
+	pub fn new(
+		print_job_id: DocID,
+		workflow_id: DocID,
+		creation_time: u32,
+		total_time_taken: u32,
+		step_times: HashMap<DocID, u32>,
+	) -> SimulationReport {
+		return SimulationReport {
+			id: None,
+			PrintJobID: print_job_id,
+			WorkflowID: workflow_id,
+			CreationTime: creation_time,
+			TotalTimeTaken: total_time_taken,
+			StepTimes: step_times,
+		};
+	}
 }
 
 // TODO: SQLize this, using HashMaps as placeholders because it's easier
@@ -109,9 +109,9 @@ static SIMULATION_REPORTS: OnceLock<Mutex<HashMap<DocID, SimulationReport>>> = O
 
 static ID_COUNTER: OnceLock<Mutex<DocID>> = OnceLock::new();
 pub fn next_id() -> DocID {
-    let mut out = ID_COUNTER.get_or_init(|| Mutex::new(0)).lock().unwrap();
-    *out += 1;
-    return *out - 1;
+	let mut out = ID_COUNTER.get_or_init(|| Mutex::new(0)).lock().unwrap();
+	*out += 1;
+	return *out - 1;
 }
 
 pub fn database_init(){
@@ -194,6 +194,28 @@ pub fn database_init(){
 		]
 	});
 
+	// Workflow with rasterization step split into different groups to be parallelized
+	let id = next_id();
+	workflows.lock().unwrap().insert(id, Workflow{
+		id: Some(id),
+		Title: "Workflow with parallel rasterization".to_string(),
+		WorkflowSteps: vec![
+			WFS { id: 2, Next: vec![3, 4], Prev: vec![] },
+			WFS { id: 3, Next: vec![5], Prev: vec![2] },
+			WFS { id: 4, Next: vec![5], Prev: vec![2] },
+
+			// Rasterization WorkflowSteps with same id to indicate parallelism
+			WFS { id: 6, Next: vec![7], Prev: vec![3, 4] }, // group 1
+			WFS { id: 6, Next: vec![7], Prev: vec![3, 4] }, // group 2
+			WFS { id: 6, Next: vec![7], Prev: vec![3, 4] }, // group 3
+
+			WFS { id: 7, Next: vec![8], Prev: vec![6] },
+			WFS { id: 8, Next: vec![9], Prev: vec![7] },
+			WFS { id: 9, Next: vec![10], Prev: vec![8] },
+			WFS { id: 10, Next: vec![], Prev: vec![9] },
+		]
+	});
+
 	let id = next_id();
 	simulation_reports.lock().unwrap().insert(id, SimulationReport{
 		id: Some(id),
@@ -208,123 +230,123 @@ pub fn database_init(){
 
 // TODO: Update to allow for querying
 pub async fn query_print_jobs() -> Result<Vec<PrintJob>, String> {
-    let print_jobs = PRINT_JOBS.get_or_init(|| Mutex::new(HashMap::new()));
-    return Ok(print_jobs.lock().unwrap().values().cloned().collect());
+	let print_jobs = PRINT_JOBS.get_or_init(|| Mutex::new(HashMap::new()));
+	return Ok(print_jobs.lock().unwrap().values().cloned().collect());
 }
 
 // TODO: Update to allow for querying
 pub async fn query_workflows() -> Result<Vec<Workflow>, String> {
-    let workflows = WORKFLOWS.get_or_init(|| Mutex::new(HashMap::new()));
-    return Ok(workflows.lock().unwrap().values().cloned().collect());
+	let workflows = WORKFLOWS.get_or_init(|| Mutex::new(HashMap::new()));
+	return Ok(workflows.lock().unwrap().values().cloned().collect());
 }
 
 // TODO: Update to allow for querying
 pub async fn query_workflow_steps() -> Result<Vec<WorkflowStep>, String> {
-    let workflow_steps = WORKFLOW_STEPS.get_or_init(|| Mutex::new(HashMap::new()));
-    return Ok(workflow_steps.lock().unwrap().values().cloned().collect());
+	let workflow_steps = WORKFLOW_STEPS.get_or_init(|| Mutex::new(HashMap::new()));
+	return Ok(workflow_steps.lock().unwrap().values().cloned().collect());
 }
 
 // TODO: Update to allow for querying
 pub async fn query_simulation_reports() -> Result<Vec<SimulationReport>, String> {
-    let simulation_reports = SIMULATION_REPORTS.get_or_init(|| Mutex::new(HashMap::new()));
-    return Ok(simulation_reports
-        .lock()
-        .unwrap()
-        .values()
-        .cloned()
-        .collect());
+	let simulation_reports = SIMULATION_REPORTS.get_or_init(|| Mutex::new(HashMap::new()));
+	return Ok(simulation_reports
+		.lock()
+		.unwrap()
+		.values()
+		.cloned()
+		.collect());
 }
 
 pub async fn find_print_job(id: DocID) -> Result<PrintJob, String> {
-    let print_jobs = PRINT_JOBS.get_or_init(|| Mutex::new(HashMap::new()));
-    return match print_jobs.lock().unwrap().get(&id) {
-        Some(data) => Ok(data.clone()),
-        None => Err("Error".to_string()),
-    };
+	let print_jobs = PRINT_JOBS.get_or_init(|| Mutex::new(HashMap::new()));
+	return match print_jobs.lock().unwrap().get(&id) {
+		Some(data) => Ok(data.clone()),
+		None => Err("Error".to_string()),
+	};
 }
 
 pub async fn find_workflow(id: DocID) -> Result<Workflow, String> {
-    let workflows = WORKFLOWS.get_or_init(|| Mutex::new(HashMap::new()));
-    return match workflows.lock().unwrap().get(&id) {
-        Some(data) => Ok(data.clone()),
-        None => Err("Error".to_string()),
-    };
+	let workflows = WORKFLOWS.get_or_init(|| Mutex::new(HashMap::new()));
+	return match workflows.lock().unwrap().get(&id) {
+		Some(data) => Ok(data.clone()),
+		None => Err("Error".to_string()),
+	};
 }
 
 pub async fn find_workflow_step(id: DocID) -> Result<WorkflowStep, String> {
-    let workflow_steps = WORKFLOW_STEPS.get_or_init(|| Mutex::new(HashMap::new()));
-    return match workflow_steps.lock().unwrap().get(&id) {
-        Some(data) => Ok(data.clone()),
-        None => Err("Error".to_string()),
-    };
+	let workflow_steps = WORKFLOW_STEPS.get_or_init(|| Mutex::new(HashMap::new()));
+	return match workflow_steps.lock().unwrap().get(&id) {
+		Some(data) => Ok(data.clone()),
+		None => Err("Error".to_string()),
+	};
 }
 
 pub async fn find_simulation_report(id: DocID) -> Result<SimulationReport, String> {
-    let simulation_reports = SIMULATION_REPORTS.get_or_init(|| Mutex::new(HashMap::new()));
-    return match simulation_reports.lock().unwrap().get(&id) {
-        Some(data) => Ok(data.clone()),
-        None => Err("Error".to_string()),
-    };
+	let simulation_reports = SIMULATION_REPORTS.get_or_init(|| Mutex::new(HashMap::new()));
+	return match simulation_reports.lock().unwrap().get(&id) {
+		Some(data) => Ok(data.clone()),
+		None => Err("Error".to_string()),
+	};
 }
 
 pub async fn insert_print_job(mut data: PrintJob) -> Result<DocID, String> {
-    if data.id != None || data.DateCreated != None {
-        return Err("Error".to_string());
-    }
-    let print_jobs = PRINT_JOBS.get_or_init(|| Mutex::new(HashMap::new()));
-    let id = next_id();
-    data.id = Some(id);
-    data.DateCreated = Some(0);
-    print_jobs.lock().unwrap().insert(id, data);
-    return Ok(id);
+	if data.id != None || data.DateCreated != None {
+		return Err("Error".to_string());
+	}
+	let print_jobs = PRINT_JOBS.get_or_init(|| Mutex::new(HashMap::new()));
+	let id = next_id();
+	data.id = Some(id);
+	data.DateCreated = Some(0);
+	print_jobs.lock().unwrap().insert(id, data);
+	return Ok(id);
 }
 
 pub async fn insert_workflow(mut data: Workflow) -> Result<DocID, String> {
-    // TODO: it would be a good idea to check that all the prev and next indices are valid
-    if data.id != None {
-        return Err("Error".to_string());
-    }
-    let workflows = WORKFLOWS.get_or_init(|| Mutex::new(HashMap::new()));
-    let id = next_id();
-    data.id = Some(id);
-    workflows.lock().unwrap().insert(id, data);
-    return Ok(id);
+	// TODO: it would be a good idea to check that all the prev and next indices are valid
+	if data.id != None {
+		return Err("Error".to_string());
+	}
+	let workflows = WORKFLOWS.get_or_init(|| Mutex::new(HashMap::new()));
+	let id = next_id();
+	data.id = Some(id);
+	workflows.lock().unwrap().insert(id, data);
+	return Ok(id);
 }
 
 pub async fn insert_simulation_report(data: SimulationReportArgs) -> Result<DocID, String> {
-    let simulation_reports = SIMULATION_REPORTS.get_or_init(|| Mutex::new(HashMap::new()));
-    let mut new_report = match simulate(data).await {
-        Ok(data) => data,
-        Err(_) => return Err("Error".to_string()),
-    };
-    let id = next_id();
-    new_report.id = Some(id);
-    simulation_reports.lock().unwrap().insert(id, new_report);
-    return Ok(id);
+	let simulation_reports = SIMULATION_REPORTS.get_or_init(|| Mutex::new(HashMap::new()));
+	let mut new_report = match simulate(data).await {
+		Ok(data) => data,
+		Err(_) => return Err("Error".to_string()),
+	};
+	let id = next_id();
+	new_report.id = Some(id);
+	simulation_reports.lock().unwrap().insert(id, new_report);
+	return Ok(id);
 }
 
 //TODO: Removing a print job should fail if any simulation reports refer to it
 pub async fn remove_print_job(id: DocID) -> Result<PrintJob, String> {
-    let print_jobs = PRINT_JOBS.get_or_init(|| Mutex::new(HashMap::new()));
-    return match print_jobs.lock().unwrap().remove(&id) {
-        Some(data) => Ok(data),
-        None => Err("Error".to_string()),
-    };
+	let print_jobs = PRINT_JOBS.get_or_init(|| Mutex::new(HashMap::new()));
+	return match print_jobs.lock().unwrap().remove(&id) {
+		Some(data) => Ok(data),
+		None => Err("Error".to_string()),
+	};
 }
 
 //TODO: Removing a workflow should fail if any simulation reports refer to it
 pub async fn remove_workflow(id: DocID) -> Result<Workflow, String> {
-    let workflows = WORKFLOWS.get_or_init(|| Mutex::new(HashMap::new()));
-    return match workflows.lock().unwrap().remove(&id) {
-        Some(data) => Ok(data),
-        None => Err("Error".to_string()),
-    };
+	let workflows = WORKFLOWS.get_or_init(|| Mutex::new(HashMap::new()));
+	return match workflows.lock().unwrap().remove(&id) {
+		Some(data) => Ok(data),
+		None => Err("Error".to_string()),
+	};
 }
 
 pub async fn remove_simulation_report(id: DocID) -> Result<SimulationReport, String> {
-    let simulation_reports = SIMULATION_REPORTS.get_or_init(|| Mutex::new(HashMap::new()));
-    return match simulation_reports.lock().unwrap().remove(&id) {
-        Some(data) => Ok(data),
-        None => Err("Error".to_string()),
-    };
+	let simulation_reports = SIMULATION_REPORTS.get_or_init(|| Mutex::new(HashMap::new()));
+	return match simulation_reports.lock().unwrap().remove(&id) {
+		Some(data) => Ok(data),
+		None => Err("Error".to_string()),
+	};
 }
