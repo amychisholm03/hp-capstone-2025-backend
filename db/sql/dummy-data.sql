9729d987453ed4030c0d71476725d0b56b507261
amychisholm03 <amychisholm2003@gmail.com>
2025-02-17 14:09:31 -0800

    fix

--- db/sql/dummy-data.sql
+++ db/sql/dummy-data.sql
@@ -7,12 +7,12 @@ INSERT INTO rasterization_profile (id, title, profile) VALUES (NULL, 'High Quali
 INSERT INTO rasterization_profile (id, title, profile) VALUES (NULL, 'Line Art (Crisp Lines, No Gradients)', 'Specialized Profile');
 
 -- updated these to match HP's example workflow
-INSERT INTO workflow_step (id,title,setup_time,time_per_page) VALUES (0, 'Download File',0,1); -- 0
-INSERT INTO workflow_step (id,title,setup_time,time_per_page) VALUES (1, 'Preflight',10,20); -- 1
-INSERT INTO workflow_step (id,title,setup_time,time_per_page) VALUES (2, 'Impose',0,5); -- 2
-INSERT INTO workflow_step (id,title,setup_time,time_per_page) VALUES (3, 'Analyzer',0,5); -- 3
-INSERT INTO workflow_step (id,title,setup_time,time_per_page) VALUES (4, 'Color Setup',2,1); -- 4
-INSERT INTO workflow_step (id,title,setup_time,time_per_page) VALUES (5, 'Rasterization',50,15); -- 5
-INSERT INTO workflow_step (id,title,setup_time,time_per_page) VALUES (6, 'Loader',100,1); -- 6
-INSERT INTO workflow_step (id,title,setup_time,time_per_page) VALUES (7, 'Cutting',10,2); -- 7
-INSERT INTO workflow_step (id,title,setup_time,time_per_page) VALUES (8, 'Laminating',10,5); -- 8
+INSERT INTO workflow_step (id,title,setup_time,time_per_page) VALUES (NULL, 'Download File',0,1); -- 0
+INSERT INTO workflow_step (id,title,setup_time,time_per_page) VALUES (NULL, 'Preflight',10,20); -- 1
+INSERT INTO workflow_step (id,title,setup_time,time_per_page) VALUES (NULL, 'Impose',0,5); -- 2
+INSERT INTO workflow_step (id,title,setup_time,time_per_page) VALUES (NULL, 'Analyzer',0,5); -- 3
+INSERT INTO workflow_step (id,title,setup_time,time_per_page) VALUES (NULL, 'Color Setup',2,1); -- 4
+INSERT INTO workflow_step (id,title,setup_time,time_per_page) VALUES (NULL, 'Rasterization',50,15); -- 5
+INSERT INTO workflow_step (id,title,setup_time,time_per_page) VALUES (NULL, 'Loader',100,1); -- 6
+INSERT INTO workflow_step (id,title,setup_time,time_per_page) VALUES (NULL, 'Cutting',10,2); -- 7
+INSERT INTO workflow_step (id,title,setup_time,time_per_page) VALUES (NULL, 'Laminating',10,5); -- 8
