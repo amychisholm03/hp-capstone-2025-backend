0b30ac3a0e906dc150eea0f13da03a5574c238ae
Amy Chisholm <amy.chisholm@wsu.edu>
2025-02-20 12:32:36 -0800

    fix: get rid of validation temporarily (#49)

--- src/database.rs
+++ src/database.rs
@@ -460,9 +460,10 @@ pub async fn insert_rasterization_profile(data: RasterizationProfile) -> Result<
 
 pub async fn insert_workflow(data: WorkflowArgs) -> Result<DocID,CustomError> {
     // Ensure that the workflow is valid
-    if !is_valid_workflow(&data) {
-        return Err(CustomError::OtherError("Invalid workflow".to_string()));
-    }
+    // TODO: uncomment once frontend can actually send valid workflows
+    // if !is_valid_workflow(&data) {
+    //     return Err(CustomError::OtherError("Invalid workflow".to_string()));
+    // }
     let db = DB_CONNECTION.lock().unwrap();
 
     // Insert the Workflow
