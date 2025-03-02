7f8576e166bd6fad34da240d935c6fc4645c1fa1
Caleb Biggs <calebjbiggs@gmail.com>
2025-03-02 13:50:01 -0800

    Finished workflow deserialization and added some comments

--- src/workflow.rs
+++ src/workflow.rs
@@ -1,13 +1,3 @@
-// #[allow(non_snake_case)]
-// #[derive(Debug, Clone, Serialize, Deserialize)]
-// pub struct Workflow {
-// 	#[serde(default)] id: Option<DocID>,
-// 	Title: String,
-// 	pub WorkflowSteps: Vec<AssignedWorkflowStep>,
-//     pub Parallelizable: bool,
-//     pub numOfRIPs: u32,
-// }
-
 use crate::database::DocID;
 use crate::workflow_steps::*;
 use serde::{Deserialize, Serialize};
@@ -15,65 +5,111 @@ use serde::de::{Deserializer, Error};
 use serde_json::{json, Value};
 
 
+/**
+ * Expected JSON input (from a POST):
+ * 	{
+ * 		"title": "Workflow Title",	// The title of the workflow
+ * 		"steps": [					// A list of steps, to be made into a graph
+ * 			{"id": 0},
+ * 			{"id": 1},
+ * 			{"id": 5, "num_cores": 7},
+ * 		]
+ * 	}
+ * 
+ * JSON output (sent in response to a GET):
+ * 	{
+ * 		"id": 0,
+ * 		"title": "Workflow Title",
+ * 		"steps": [
+ * 			{
+ * 				"data": {"id": 0, "title": "Download File", "setup_time": 0, "time_per_page": 1},
+ * 				"prev": [],
+ * 				"next": [1]
+ * 			},{
+ * 				"data": {"id": 1, "title": "Preflight", "setup_time": 10, "time_per_page": 20},
+ * 				"prev": [0],
+ * 				"next": [2]
+ * 			},{
+ * 				"data": {"id": 5, "title": "Rasterization", "setup_time": 50, "time_per_page": 15, "num_cores": 7},
+ * 				"prev": [1],
+ * 				"next": []
+ * 			},
+ * 		]
+ * 	}
+ **/
+
+
 #[derive(Debug, Clone, Serialize)]
 struct WorkflowNode {
 	data: WFSVariant,
-	prev: Vec<usize>,
-	next: Vec<usize>
+	prev: Vec<usize>, // List of indices corresponding to previous nodes
+	next: Vec<usize>  // List of indices corresponding to subsequent nodes
 }
 
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct Workflow {
-	id: DocID,
+	#[serde(default)] 
+	id: Option<DocID>,
 	title: String,
 	#[serde(deserialize_with="deserialize_steps")]
 	steps: Vec<WorkflowNode>
 }
 
 
-// struct WorkflowArgs {
-// 	title: String,
-// 	steps: Vec<WFSVariant>
-// }
-
-
 fn deserialize_steps<'de, D>(deserializer: D) -> Result<Vec<WorkflowNode>, D::Error>
 where D: Deserializer<'de> {
 	let json_vector: Vec<Value> = Deserialize::deserialize(deserializer)?;
-	dbg!(json_vector.clone());
-
-	// let steps: Vec<WorkflowNode> = 
+	let mut steps = Vec::<WorkflowNode>::new();
 	for object in json_vector {
-		let obj: WFSVariant = serde_json::from_value(object)
-			.map_err(|_| Error::custom(format!("TODO")))?;
-		dbg!(obj);
+		steps.push(WorkflowNode {
+			data: serde_json::from_value(object)
+				.map_err(|_| Error::custom(format!("TODO")))?,
+			prev: vec![],
+			next: vec![]
+		});
 	}
-
-	return Ok(vec![WorkflowNode{data: WFSVariant::DownloadFile, prev: vec![], next: vec![]}]);
+	
+	return Ok(fill_edges(steps));
 }
 
 
-pub fn wf_test() {
-	// let wf = Workflow {
-	// 	id: 5,
-	// 	title: "Test Workflow".to_string(),
-	// 	steps: vec![
-	// 		WorkflowNode { 
-	// 			data: WFSVariant::DownloadFile,
-	// 			prev: vec![],
-	// 			next: vec![1]
-	// 		},
-	// 		WorkflowNode {
-	// 			data: WFSVariant::Rasterization {num_cores: 7},
-	// 			prev: vec![0],
-	// 			next: vec![]
-	// 		}
-	// 	]
-	// };
-	// dbg!(wf.clone());
-	// println!("{}", json!(wf));
+// Given a list of nodes with no edges, fill in the edges to create a graph
+fn fill_edges(steps: Vec<WorkflowNode>) -> Vec<WorkflowNode> {
+	// For Amy
+	return steps;
+}
 
-	let data = "{\"id\": 5, \"title\": \"Test Workflow 2\", \"steps\": [{\"id\": 0}, {\"id\": 1}, {\"id\": 5, \"num_cores\": 3}]}";
+
+// Testing serialization and deserialization
+pub fn wf_test() {
+	let wf = Workflow {
+		id: Some(5),
+		title: "Test Workflow".to_string(),
+		steps: vec![
+			WorkflowNode { 
+				data: WFSVariant::DownloadFile,
+				prev: vec![],
+				next: vec![1]
+			},
+			WorkflowNode {
+				data: WFSVariant::Rasterization {num_cores: 7},
+				prev: vec![0],
+				next: vec![]
+			}
+		]
+	};
+	println!("Serialized:\n{}\n", json!(wf));
+
+
+	let data = "{
+		\"title\": \"Test Workflow 2\", 
+		\"steps\": [
+			{\"id\": 0}, 
+			{\"id\": 1}, 
+			{\"id\": 5, \"num_cores\": 3}
+		]
+	}";
+	println!("Deserialized:");
 	dbg!(serde_json::from_str::<Workflow>(data));
 }
